66d480153d1794eec91a6631884807e7a46f2ec6
Loredana <loredana.cirstea@gmail.com>
2020-06-05 01:33:53 +0300

    Fix web3 connection
    fixes https://github.com/loredanacirstea/taylor/issues/40

--- web/src/utils/web3.js
+++ web/src/utils/web3.js
@@ -1,33 +1,20 @@
 import { ethers } from 'ethers';
 
-const getWeb3 = async () => {
-    let web3Instance;
+const connect = async () => {
     if (window.ethereum) {
-        // eslint-disable-next-line
-        web3Instance = new Web3(ethereum);
         try {
             // eslint-disable-next-line
             await ethereum.enable();
         } catch (error) {
             console.log('User rejected dApp connection');
         }
-    }
-
-    if (!web3Instance && window.web3) {
-        // eslint-disable-next-line
-        web3Instance = new Web3(web3.currentProvider);
-    }
-
-    if (!web3Instance) {
-        console.log('Non-Ethereum browser detected. Consider trying MetaMask!');
-        return null;
-    }
-    return web3Instance;
+    } 
 };
 
 const getProvider = async () => {
-  const web3 = await getWeb3();
-  if (web3) {
+  await connect();
+  if (window.web3 && window.web3.currentProvider) {
+    // eslint-disable-next-line
     const provider = new ethers.providers.Web3Provider(web3.currentProvider);
     const signer = provider.getSigner(0);
     return { provider, signer };
@@ -36,6 +23,5 @@ const getProvider = async () => {
 }
 
 export {
-  getWeb3,
   getProvider,
 }
