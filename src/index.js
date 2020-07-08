4e6c7f36084ca50199b8df53f915c8eddcd19c16
Loredana <loredana.cirstea@gmail.com>
2020-07-08 22:19:43 +0300

    Deployment API for Taylor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const taylor = require('./taylor.js');
-const bootstrap = require('./bootstrap.js');
+const bootstrap_functions = require('./bootstrap.js');
+const deploy = require('./deploy.js');
 
-taylor.bootstrap = bootstrap;
+taylor.bootstrap_functions = bootstrap_functions;
+taylor.deploy = deploy.deploy;
+taylor.bootstrap = deploy.bootstrap;
 module.exports = taylor;
