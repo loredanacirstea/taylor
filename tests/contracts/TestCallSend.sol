f23e2df11939545bf60f9dca1bc6b00bc838e29b
Loredana <loredana.cirstea@gmail.com>
2020-08-11 01:40:50 +0300

    call with tuple

--- tests/contracts/TestCallSend.sol
+++ tests/contracts/TestCallSend.sol
@@ -1,8 +1,22 @@
 pragma solidity >=0.4.22 <0.7.0;
+pragma experimental ABIEncoderV2;
 
 contract TestCallSend {
     uint public somevar = 5;
     string public name = "SomeName";
+
+    struct TT1 {
+        bytes a;
+        string b;
+        uint c;
+    }
+
+    struct TT2 {
+        uint a;
+        uint b;
+        TT1 c;
+        uint d;
+    }
 
     function add(uint a, uint b) public pure returns(uint c) {
         c = a + b;
@@ -18,5 +32,14 @@ contract TestCallSend {
 
     function pay(uint a) public payable {
         somevar = somevar + a;
+    }
+
+    function testTuple(TT2 memory tt2) public pure returns(uint) {
+        return tt2.a + tt2.b + tt2.d + tt2.c.c;
+    }
+
+    function getaTuple(bytes memory a, string memory b, uint c) public pure returns(TT2 memory) {
+        TT1 memory tup = TT1(a, b, c);
+        return TT2(5, 5, tup, 10);
     }
 }
